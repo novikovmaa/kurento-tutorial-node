458e91c2be2b21b4bc9349f343ddfee09d05eb38
Mikhail Novikov <novikovmaa@gmail.com>
2016-06-11 09:10:54 +0000

    recordings

--- kurento-one2many-call/config.js
+++ kurento-one2many-call/config.js
@@ -1,5 +1,5 @@
 {
-	"mongo_dsn": "mongodb://demio:Kdy4Ga41H3kje2lPpVrk2GkLW0Foqe7D@104.236.49.21:27017/recordings",
-	"server_alias": "StagingX"
+	"mongo_dsn": "__mongo_dsn__",
+	"server_alias": "__server_alias__"
 }
 
